4dbdde043e639b3698974a154c9519508dd366ca
TehanPemasiri <113910953+TehanPemasiri@users.noreply.github.com>
2025-01-04 19:13:17 +0530

    updated hooks.

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
     features = "src/test/resources/Features", // Path to feature files
-    glue = "stepDefinitions", // Path to step definition classes
+    glue = {"stepDefinitions","hooks"}, // Path to step definition classes
     plugin = {"pretty", "html:target/cucumber-report.html"} // Report options
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
